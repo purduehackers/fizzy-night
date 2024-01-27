afbb62010ffdfa1b6ad5d3b4f0f28ff590a606e0
Matthew Stanciu <mattbstanciu@gmail.com>
2024-01-26 19:42:28 -0500

    3.14

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import {
-    DoorbellCard,
-    DoorbellContext,
-} from "@/components/Doorbell/doorbell";
+import { DoorbellCard, DoorbellContext } from "@/components/Doorbell/doorbell";
 import { FC, useState } from "react";
 import { ConfettiCannon } from "@/components/ConfettiCannon/confetticannon";
 import { Info } from "@/components/InfoCard/info";
@@ -25,18 +22,20 @@ const MainPage: FC = () => {
         <main className="overflow-hidden select-none">
             <div className={`w-screen h-screen bg-black`}>
                 <div className={`w-screen h-screen flex flex-col`}>
-                    <div className={`flex flex-row grow justify-stretch max-h-[calc(100vh-140px)]`}>
-                        <DashboardLunarLander/>
-                        <DiscordFeed/>
+                    <div
+                        className={`flex flex-row grow justify-stretch max-h-[calc(100vh-140px)]`}
+                    >
+                        <DashboardLunarLander />
+                        <DiscordFeed />
                     </div>
 
                     <div className={`flex flex-row  h-[140px] justify-between`}>
                         <Spotify />
                         <Info
                             title={"HACK NIGHT"}
-                            tagline={"COLD AF"}
-                            taglineColour={"blue"}
-                            version={"3.13"}
+                            tagline={"PI(E)!!!"}
+                            taglineColour={"yellow"}
+                            version={"3.14"}
                         />
                     </div>
                 </div>
