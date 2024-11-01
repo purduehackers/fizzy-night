7c02ab5d719af832a18313ada2da7ba4c0546929
Matthew Stanciu <mattbstanciu@gmail.com>
2024-11-01 15:53:01 -0400

    5.9

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,9 +45,9 @@ const MainPage: FC = () => {
                         <Spotify />
                         <Info
                             title={"HACK NIGHT"}
-                            tagline={"spooky scary spirits, aperiodic tile!"}
-                            taglineColour={"blue"}
-                            version={"5.8"}
+                            tagline={"now with more metal"}
+                            taglineColour={"gray"}
+                            version={"5.9"}
                         />
                     </div>
                 </div>
